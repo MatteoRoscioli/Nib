84455215549c54720e2b0ded24e85dc4564abda7
MatteoRoscioli <165591001+MatteoRoscioli@users.noreply.github.com>
2026-02-08 10:56:25 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
 # hello.py - Simple hello world
-print("Hello from Nib OS!")
-print("Running on bare metal!")
+print("Hello from MicroPython on Nib OS!")
+print("This is running on bare metal Raspberry Pi!")
+print("")
+print("System info:")
+print("- No underlying OS")
+print("- Direct hardware access")
+print("- Python powered!")
